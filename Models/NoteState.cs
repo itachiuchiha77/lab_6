51d8f7833dfcbba283a93efd19a171308fe40e25
itachiuchiha77 <82699311+itachiuchiha77@users.noreply.github.com>
2022-03-24 23:52:33 +0700

    Update NoteState.cs

--- Models/NoteState.cs
+++ Models/NoteState.cs
@@ -4,15 +4,7 @@ namespace lab_6.Models
 {
     public class NoteState
     {
-/*        private static string _path = ""; // Path to DataBase where we have our Notes
-        public static void SetPathToFile(string PathToFile) { _path = PathToFile; }
-        public static string GetPathToFile() { return _path; }*/
-
-/*        private int _uid; // Note id // Format - from 0 to 999 // Not sure that it will bigger than 10 // Example if id: 1, 2, 3 and etc... //
-        public void SetUID(int uid) { _uid = uid; }
-        public int GetUID() { return _uid; }*/
-
-        public string Title { get; set; } // Name of Note, Default Name = "Note #{uid}"
+        public string Title { get; set; } 
         public string Text { get; set; }
         public DateTimeOffset Date { get; set; }
 
@@ -22,12 +14,5 @@ namespace lab_6.Models
             Text = description;
             Title = name;
         }
-
-/*        public NoteState(int id, string notetxt, string name)
-        {
-            //SetUID(id);
-            SetNameOfNote(name);
-            Text = notetxt;
-        }*/
     }
 }
